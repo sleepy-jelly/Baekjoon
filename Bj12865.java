efc8a01e2a0d2e976a13c3a9f906150125d2f7ce
zayle_nothing <57306207+zayle-nothing@users.noreply.github.com>
2022-09-26 12:28:10 +0900

    Update Bj12865.java

--- Bj12865.java
+++ Bj12865.java
@@ -1,54 +1,49 @@
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Comparator;
-public class Bj12865 {
 
+import java.util.Scanner;
+public class Bj12865 {
+	
+	static Integer[][] dp;
+	static int[] w;
+	static int[] v;
+	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc= new Scanner(System.in);
 		
-		int newTotalStuff =sc.nextInt();
+		int totalStuff =sc.nextInt();
 		int maximumWeight =sc.nextInt();
-		int currentValue = 0;
-		int[] maximumValue = new int[newTotalStuff];
-		int currentWeight = 0;
-		int[][] arrData1 = new int[newTotalStuff][2];		
-		for(int i=0;i<arrData1.length;i++) {  
-			for(int j=0;j<1;j++) { // j == weight  j+1 == value of the thing 
-				arrData1[i][j]= sc.nextInt();
-				arrData1[i][j+1]=sc.nextInt();
-				System.out.print(arrData1[i][j]+" ");
-				System.out.println(arrData1[i][j+1]);
-			}
-		}
-		// me personally if i do some sort array it will be easier 
-		Arrays.sort(arrData1, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[0]!=o2[0] ? o1[0]-o2[0] : o1[1]-o2[1];
-			}
-		});
-		for(int i=0;i<arrData1.length;i++) {  
-			for(int j=0;j<1;j++) { // j == weight  j+1 == value of the thing 
-				if (maximumWeight<(arrData1[i][j]+currentValue)) {
-					currentWeight = currentWeight +arrData1[i][j];
-					currentValue = currentValue + arrData1[i][j+1];
-				}
-				
-				System.out.print(arrData1[i][j]+" ");
-				System.out.println(arrData1[i][j+1]);
-			}
-			
+		
+		w=new int [totalStuff];
+		v=new int [maximumWeight];
+		
+		dp = new Integer[totalStuff][maximumWeight+1];
+		
+		
+		for(int i =0; i<totalStuff;i++) {
+			w[i]= sc.nextInt();
+			v[i]= sc.nextInt();
 		}
 		
 		
-		
-		
-		
-		
-		System.out.println(maximumValue);
+		System.out.println(dp(totalStuff-1,maximumWeight));
+
 		
 		sc.close();
-
 	}
+	
+	static int dp(int i, int j) {
+		if(i<0) {             // if  i is lower than 0 (if i is out of data list)
+			return 0;
+		}
+		
+		if(dp[i][j]==null) {  // if there is no data 
+			
+			if(w[i]>j) {
+				dp[i][j]=dp(i-1, j);
+			}else {
+				dp[i][j]= Math.max(dp(i-1,j), dp(i-1,j-w[i])+v[i]);
 
+			}
+		}	
+		return dp[i][j];
+	}
 }
