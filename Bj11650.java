b3aab4c1bc38855e57e4624300f28347fe2ef793
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2022-10-12 17:44:04 +0900

    Add files via upload

---
+++ Bj11650.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Bj11650 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		
+		int nMax =sc.nextInt();
+		int[][] arr= new int[nMax][2];
+		for(int i =0;i<nMax;i++) {
+			
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+			
+				
+			
+		}
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
