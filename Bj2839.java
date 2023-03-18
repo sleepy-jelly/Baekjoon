df9e4b03acfc0350ba61d9549705af93a66c507a
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2023-03-18 13:03:23 +0900

    Add files via upload

--- Bj2839.java
+++ Bj2839.java
@@ -1,72 +1,30 @@
-import java.util.Scanner;
-public class Bj2839 {
+package Baekjoon;
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int nWeight= sc.nextInt();
-	
-		int n3kgBag =0;
-		int n5kgBag =0;
-		
-		n5kgBag =(nWeight/15)*3;
-		nWeight=nWeight%15;
-		
-		switch(nWeight) {
-			case 0:
-				break;
-			case 1:
-			case 2:
-			case 4:
-			case 7:
-				n3kgBag=-1;
-				n5kgBag=0;
-				break;
-			case 3:
-				n3kgBag =1;
-				break;
-			case 5:
-				n5kgBag =n5kgBag+1;
-				break;
-			case 6:
-				n3kgBag =2;
-				break;
-			case 8:
-				n3kgBag = 1;
-				n5kgBag = n5kgBag +1;
-				break;
-			case 9:
-				n3kgBag= 3;
-				break;
-			case 10: 
-				n5kgBag = n5kgBag+2;
-				break;
-			case 11:
-				n3kgBag = 2;
-				n5kgBag = n5kgBag+1;
-				break;
-			case 12:
-				n3kgBag = 4;
-				break;
-			case 13:
-				n3kgBag =1;
-				n5kgBag = n5kgBag+2;
-				break;
-			case 14:
-				n3kgBag = 3;
-				n5kgBag = n5kgBag +1;
-				break;
-		}
-		
-		System.out.println(n5kgBag+n3kgBag);
-		
-		
-		
-		
-		
-		
-		sc.close();
-	}
+import java.util.Scanner;
+
+
+public class Bj2839{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int totalSugar  =sc.nextInt();
+
+        if(totalSugar==4||totalSugar==7){
+            System.out.println(-1);
+        }else if(totalSugar%5==0){
+            System.out.println(totalSugar/5);
+        }else if(totalSugar%5==1||totalSugar%5==3){
+            System.out.println((totalSugar/5)+1);
+        }else if(totalSugar%5==2||totalSugar%5==4){
+            System.out.println((totalSugar/5)+2);
+        }
+
+
+
+
+
+
+        sc.close();
+    }
 
 }
