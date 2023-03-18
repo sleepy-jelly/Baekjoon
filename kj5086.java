dfc4854ac7ffde823d00d50269b9fe69d4c5a1eb
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2023-03-18 13:13:56 +0900

    Add files via upload

---
+++ kj5086.java
@@ -0,0 +1,39 @@
+package Baekjoon;
+import java.util.Scanner;
+
+
+public class kj5086 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int nData1 =1;
+        int nData2 =1;
+
+
+        do{
+
+
+
+
+        }while(!((nData1==0)&&(nData2==0)));
+
+       
+
+        
+
+
+
+
+
+
+
+
+
+        sc.close();
+
+
+
+
+    }
+    
+}
