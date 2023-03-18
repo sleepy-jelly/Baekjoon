64fc5d75011ab1a4214892213999603a7a5e4526
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2023-03-18 13:03:38 +0900

    Add files via upload

---
+++ kj1018.java
@@ -0,0 +1,22 @@
+package Baekjoon;
+import java.util.Scanner;
+
+
+public class kj1018 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        
+
+            
+
+
+
+
+
+
+        sc.close();
+    }
+    
+    
+}
