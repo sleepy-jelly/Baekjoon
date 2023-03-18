e262e241e45436d818e27658821fe29a1ab4d7ba
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2023-03-18 13:15:05 +0900

    Add files via upload

---
+++ kj25314.java
@@ -0,0 +1,37 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class kj25314 {
+    public static void main(String[] args){
+
+        Scanner sc= new Scanner(System.in);
+
+        int nValue;
+        String RESULT="";
+        nValue=sc.nextInt();
+
+
+        for(int i=0; i<nValue/4;i++){
+
+            RESULT=RESULT+"long ";
+
+
+
+
+
+        }
+
+
+
+        RESULT=RESULT+"int";
+
+
+        System.out.println(RESULT);
+
+
+
+        sc.close();
+    }
+    
+}
